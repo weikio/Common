462c0a224145d8f3c1faa819c972edff44274202
Mikael Koskinen <mikael.koskinen@adafy.com>
2021-09-22 14:14:22 +0300

    Moved to main brancj

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weik.io Common
+
+Common code and libraries used by the other Weik.io projects.
+
+This project will be depricated and it isn't maintained.
---
+++ src/Weikio.AspNetCore.StartupTasks/StartupTaskExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Weikio.Common;
+
+namespace Weikio.AspNetCore.StartupTasks
+{
+    public static class StartupTaskExtensions
+    {
+        private static bool _healthChecksAdded = false;
+        private static readonly StartupTaskContext _sharedContext = new StartupTaskContext();
+
+        public static IServiceCollection AddStartupTask<T>(this IServiceCollection services)
+            where T : class, IStartupTask
+        {
+            return services.AddStartupTask(typeof(T));
+        }
+
+        public static IServiceCollection AddStartupTask(this IServiceCollection services, Type startupTaskType)
+        {
+            services.AddTransient(startupTaskType);
+
+            return services;
+        }
+
+        public static IServiceCollection AddStartupTasks(this IServiceCollection services, bool autoRegisterStartupTasks = true, StartupTasksHealthCheckParameters healthCheckParameters = null)
+        {
+            services.TryAddSingleton(_sharedContext);
+            services.TryAddSingleton(services);
+
+            if (healthCheckParameters == null)
+            {
+                healthCheckParameters = new StartupTasksHealthCheckParameters();
+            }
+
+            services.TryAddSingleton(healthCheckParameters);
+            
+            if (healthCheckParameters.IsEnabled && _healthChecksAdded == false)
+            {
+                services
+                    .AddHealthChecks()
+                    .AddCheck<StartupTasksHealthCheck>(healthCheckParameters.HealthCheckName);
+
+                _healthChecksAdded = true;
+            }
+
+            services.TryAddEnumerable(ServiceDescriptor.Singleton<IStartupFilter, StartupTasksStartupFilter>());
+            services.AddHostedService<StartupTaskHostedService>();
+            services.TryAddSingleton<IStartupTaskQueue, StartupTaskQueue>();
+
+            if (!autoRegisterStartupTasks)
+            {
+                return services;
+            }
+
+            RegisterStartupTasks(services);
+
+            return services;
+        }
+
+
+        private static void RegisterStartupTasks(IServiceCollection services)
+        {
+            var startupTaskTypes = TypeLocator.LocateTypesByInterface(typeof(IStartupTask));
+            foreach (var startupTaskType in startupTaskTypes)
+            {
+                services.AddStartupTask(startupTaskType);
+            }
+        }
+    }
+}
